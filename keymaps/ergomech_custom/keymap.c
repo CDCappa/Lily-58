5eecba206c51d65ae2e060d4da4d40cc47fa25df
cdCappa <carlos.cappa@trickgs.com>
2025-11-28 00:38:50 -0300

    Fix: Correct LAYOUT to 58 keys (was 60)

--- keymaps/ergomech_custom/keymap.c
+++ keymaps/ergomech_custom/keymap.c
@@ -1,11 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// ============================================
-// CUSTOM KEYMAP PARA LILY58 - ERGOMECH STORE
-// Convertido desde VIA layout JSON
-// Layout: English International
-// ============================================
-
 enum layers {
     _BASE = 0,
     _LAYER1,
@@ -21,23 +15,14 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE - Layer 0 (desde VIA JSON)
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  @   |  %   |  [   |  ]   |  '   |  |   |                    |  `   |  F2  |  F4  |  F5  |  ;   |  .   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |  ,   |  =   |
- * |------+------+------+------+------+------|  NO   |    |  MO1  |------+------+------+------+------+------|
- * |Shift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |  NO  |  &   |  *   |  NO  |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | GUI  |MO(2) | Alt  | /Space  /       \Enter \  | Esc  |Bkspc |  /   |  !   |  #   |
+ * Lily58 tiene 58 teclas: 4 filas de 6 + 2 thumb inner + 4 thumb outer por lado
  */
 [_BASE] = LAYOUT(
   S(KC_2),  S(KC_5),  KC_LBRC,  KC_RBRC,  KC_QUOT,  S(KC_BSLS),                     KC_GRV,   KC_F2,    KC_F4,    KC_F5,    KC_SCLN,  KC_DOT,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                           KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MINS,
   KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,     KC_L,     KC_COMM,  KC_EQL,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_NO,      MO(1),    KC_N,     KC_M,     KC_NO,    S(KC_7),  S(KC_8),  KC_NO,
-                                KC_LGUI,  MO(2),    KC_LALT,  KC_SPC,     KC_ENT,   KC_ESC,   KC_BSPC,  KC_SLSH,  S(KC_1),  S(KC_3)
+                                KC_LGUI,  MO(2),    KC_LALT,  KC_SPC,     KC_ENT,   KC_BSPC,  KC_SLSH,  KC_ESC
 ),
 
 /* LAYER1 - Simbolos */
@@ -46,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,    KC_BSLS,  S(KC_4),  S(KC_6),  KC_JYEN,  KC_NO,                          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO
+                                KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO
 ),
 
 /* LAYER2 - F-keys y navegacion */
@@ -55,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                          KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
   _______,  _______,  _______,  _______,  _______,  _______,                        KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,
   _______,  _______,  _______,  _______,  _______,  _______,  KC_NO,      KC_BSLS,  S(KC_EQL),KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_NO,
-                                _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______
+                                _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
 ),
 
 /* ADJUST - Layer1 + Layer2 */
@@ -64,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                          KC_NO,    KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_NO,    KC_NO,
   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    MC_ENE,   KC_MPRV,  KC_MPLY,  KC_MNXT,  MC_ENEM,  KC_NO,
-                                _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______
+                                _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
 )
 };
 
